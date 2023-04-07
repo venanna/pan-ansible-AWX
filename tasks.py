0aadd114ca662e20204dab22197280b252c612f1
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-07 06:02:20 -0500

    more extensive invoke options

--- tasks.py
+++ tasks.py
@@ -1,26 +1,71 @@
-"""Tasks for use with Invoke.
+"""Tasks to execute with Invoke."""
 
-(c) 2021 Calvin Remsburg
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
+# ---------------------------------------------------------------------------
+# standard library
+# ---------------------------------------------------------------------------
+import inspect
 import os
+import platform
+
+# ---------------------------------------------------------------------------
+# third party
+# ---------------------------------------------------------------------------
+import distro
 from invoke import task
 
-# ---------------------------------------------------------------------------
-# DOCKER PARAMETERS
-# ---------------------------------------------------------------------------
-DOCKER_IMG = "ghcr.io/cdot65/pan-os-docker"
-DOCKER_TAG = "ansible"
 
+# ---------------------------------------------------------------------------
+# Python3.11 hack for invoke
+# ---------------------------------------------------------------------------
+if not hasattr(inspect, "getargspec"):
+    inspect.getargspec = inspect.getfullargspec
+
+
+# ---------------------------------------------------------------------------
+# Determine CPU architecture
+# ---------------------------------------------------------------------------
+def get_os_architecture():
+    arch = platform.machine()
+
+    if (
+        "x86" in arch
+        or "i386" in arch
+        or "i686" in arch
+        or "AMD64" in arch
+        or "amd64" in arch
+    ):
+        return "x86"
+    elif "arm" in arch or "aarch64" in arch:
+        return "ARM"
+    else:
+        return "Unknown"
+
+
+CPU_ARCHITECTURE = get_os_architecture()
+
+
+# ---------------------------------------------------------------------------
+# Determine Host operating system (to use Docker or Podman)
+# ---------------------------------------------------------------------------
+def get_container_runtime():
+    dist = distro.id()
+
+    if "rhel" in dist or "fedora" in dist or "centos" in dist:
+        return "podman"
+    else:
+        return "docker"
+
+
+CONTAINER_RUNTIME = get_container_runtime()
+
+# ---------------------------------------------------------------------------
+# CONTAINER PARAMETERS
+# ---------------------------------------------------------------------------
+CONTAINER_IMG = "ghcr.io/cdot65/pan-os-docker"
+
+ARM_TAG = "arm"
+X86_TAG = "x86"
+VERSION = "ansible"
 
 # ---------------------------------------------------------------------------
 # SYSTEM PARAMETERS
@@ -29,30 +74,93 @@ PWD = os.getcwd()
 
 
 # ---------------------------------------------------------------------------
-# DOCKER CONTAINER SHELL
+#  CONTAINER BUILDS
 # ---------------------------------------------------------------------------
-@task
-def shell(context):
-    """Get access to the bash shell within our container."""
-    context.run(
-        f"docker run -it --rm \
-            -v {PWD}/ansible:/home/ansible \
-            -w /home/ansible/ \
-            {DOCKER_IMG}:{DOCKER_TAG} /bin/sh",
-        pty=True,
-    )
+@task(optional=["arm", "x86"])
+def build(context, arm=None, x86=None):
+    """Build our Container images."""
+    if CPU_ARCHITECTURE == "ARM" or arm:
+        context.run(
+            f"{CONTAINER_RUNTIME} build \
+                -t {CONTAINER_IMG}:{ARM_TAG}-{VERSION} \
+                docker/arm",
+        )
+    elif CPU_ARCHITECTURE == "x86" or x86:
+        context.run(
+            f"{CONTAINER_RUNTIME} build \
+                -t {CONTAINER_IMG}:{X86_TAG}-{VERSION} \
+                .",
+        )
+    else:
+        context.run(
+            f"{CONTAINER_RUNTIME} build \
+                -t {CONTAINER_IMG}:{X86_TAG}-{VERSION} \
+                .",
+        )
 
 
 # ---------------------------------------------------------------------------
-# EXECUTE ANSIBLE PLAYBOOK
+# SHELL ACCESS
 # ---------------------------------------------------------------------------
-@task
-def ansible(context):
-    """Test the playbook by running it within the container."""
-    context.run(
-        f"docker run -it --rm \
-            -v {PWD}/ansible:/home/ansible \
-            -w /home/ansible/ \
-            {DOCKER_IMG}:{DOCKER_TAG} ansible-playbook hello.yaml",
-        pty=True,
-    )
+@task(optional=["arm", "x86"])
+def shell(context, arm=None, x86=None):
+    """Get shell access to the container."""
+    if CPU_ARCHITECTURE == "x86" or x86:
+        context.run(
+            f"{CONTAINER_RUNTIME} run -it --rm \
+                -v {PWD}/docker/.zshrc:/home/ansible/.zshrc \
+                -v {PWD}/ansible/playbooks:/home/ansible/playbooks \
+                -w /home/ansible/playbooks \
+                {CONTAINER_IMG}:{ARM_TAG}-{VERSION} \
+                /bin/zsh",
+            pty=True,
+        )
+    elif CPU_ARCHITECTURE == "ARM" or arm:
+        context.run(
+            f"{CONTAINER_RUNTIME} run -it --rm \
+                -v {PWD}/docker/.zshrc:/home/ansible/.zshrc \
+                -v {PWD}/ansible/:/home/ansible/playbooks \
+                -w /home/ansible/playbooks \
+                {CONTAINER_IMG}:{ARM_TAG}-{VERSION} \
+                /bin/zsh",
+            pty=True,
+        )
+    else:
+        context.run(
+            f"{CONTAINER_RUNTIME} run -it --rm \
+                -v {PWD}/ansible:/home/ansible \
+                -w /home/ansible/ \
+                {CONTAINER_IMG}:{X86_TAG}-{VERSION} \
+                /bin/sh",
+            pty=True,
+        )
+
+
+# ---------------------------------------------------------------------------
+# Run playbook on localhost within container
+# ---------------------------------------------------------------------------
+@task(optional=["arm"])
+def ansible(context, arm=None):
+    """Run Ansible playbook."""
+    if CPU_ARCHITECTURE == "ARM" or arm:
+        context.run(
+            f"{CONTAINER_RUNTIME} run -it --rm \
+                -v {PWD}/ansible:/home/ansible \
+                -w /home/ansible/ \
+                {CONTAINER_IMG}:{ARM_TAG}-{VERSION} \
+                ansible-playbook hello.yaml \
+                --inventory inventory/localhost.yaml \
+                --verbose",
+            pty=True,
+        )
+    else:
+        context.run(
+            f"{CONTAINER_RUNTIME} run -it --rm \
+                -v {PWD}/ansible:/home/ansible \
+                -w /home/ansible/ \
+                {CONTAINER_IMG}:{X86_TAG}-{VERSION} \
+                ansible-playbook hello.yaml \
+                --inventory inventory/localhost.yaml \
+                --verbose",
+            pty=True,
+        )
